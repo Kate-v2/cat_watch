e3327e5a36fd8314c87f7f6d13be31713e613611
Katelyn <katelyn.rimkunas@gmail.com>
2018-10-16 11:42:52 -0600

    google enums

--- app/controllers/wild/cats_controller.rb
+++ app/controllers/wild/cats_controller.rb
@@ -2,4 +2,15 @@ class Wild::CatsController < ApplicationController
   def index
     @cats = Cat.where(status: 1)
   end
+
+  def show
+    cat = Cat.find(params[:id])
+    if cat.status == 'wild'
+      @cat = cat
+    else
+      redirect_to wild_cats_path
+    end
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
 
+  resources :owners, only: [:index] do
+    resources :cats,   only: [:index, :show]
+  end
+
+  namespace :wild do
+	 resources :cats, only: [:index, :show]
+	end
+
 end
